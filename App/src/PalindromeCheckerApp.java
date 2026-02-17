ba50c1a17752c29e8f96011f5c963414a680f0ca
ssarode2506 <ssarode2506@gmail.com>
2026-02-17 15:24:30 +0530

    [Ronith] Completed UC1

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,22 +2,9 @@ import java.util.Scanner;
 
 public class PalindromeCheckerApp{
     public static void main(String[] args){
-        String palindrome;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Palindrome to be checked: ");
-        palindrome = sc.nextLine();
-        String rev_palindrome;
-        int count = 0;
-        for(int i = 0; i < palindrome.length();i++){
-            if( palindrome.charAt(i) == palindrome.charAt(palindrome.length()-i-1)){
-                count++;
-            }
-        }
-        if(count == palindrome.length()){
-            System.out.println(palindrome + " is a palindrome: ");
-        }else{
-            System.out.println(palindrome + " is not a palindrome");
-        }
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System Initialized successfully");
 
     }
 
