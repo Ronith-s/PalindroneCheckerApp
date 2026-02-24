4158caee4d09b2b361d0bcc5ab1dac30fa6b4f0f
Ronith-s <ronith.samdariya@gmaill.com>
2026-02-24 16:12:25 +0530

    [Ronith] Completed UC6

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,24 +1,36 @@
 import java.util.Scanner;
+import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String palindrome;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Input Text: ");
-        palindrome = sc.nextLine();
-        String rev_palindrome;
-        int count = 0;
-        for (int i = 0; i < palindrome.length(); i++) {
-            if (palindrome.charAt(i) == palindrome.charAt(palindrome.length() - i - 1)) {
-                count++;
+        System.out.println("Input:");
+        String input = sc.nextLine();
+
+        // Queue for FIFO
+        Queue<Character> queue = new LinkedList<>();
+        // Stack for LIFO
+        Stack<Character> stack = new Stack<>();
+
+        // Enqueue and push characters
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            queue.add(c);   // enqueue
+            stack.push(c);  // push
+        }
+
+        // Compare dequeue vs pop
+        boolean isPalindrome = true;
+        while (!queue.isEmpty() && !stack.isEmpty()) {
+            if (!queue.remove().equals(stack.pop())) {
+                isPalindrome = false;
+                break;
             }
         }
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
 
-        boolean is_palindrome = false;
-        if (count == palindrome.length()) {
-            is_palindrome = true;
-        }
-        System.out.println("Is it a Palindrome? : "+is_palindrome);
-
+        sc.close();
     }
 }
